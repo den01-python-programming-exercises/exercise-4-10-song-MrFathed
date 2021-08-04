85a684f07e0c0a55bf40b6b179e278ba9776179b
Justin Turner <jmturner638@gmail.com>
2021-08-04 07:10:49 -0500

    First Try

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,10 @@
+from song import Song
+
 def main():
     #write your code below this line
+    garden = Song("In The Garden", 10910)
+    print("The song " + garden.name + " has a length of " + str(garden.length) + " seconds.")
+
 
 if __name__ == '__main__':
     main()
